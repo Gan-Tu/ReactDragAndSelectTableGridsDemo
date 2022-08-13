70e615c5bf03b5f4463023b22e64bc240fa1af3e
Gan Tu <github@tugan.dev>
2022-08-13 10:15:11 -0400

    add license

--- src/state/redux/cube/index.js
+++ src/state/redux/cube/index.js
@@ -1,22 +1,25 @@
-// import { call, put, takeEvery } from "redux-saga/effects";
-
-// function* fetchUser({ username }) {
-//   const resp = yield call(
-//     fetch,
-//     `http://localhost:8080/users?username=${username}&_limit=1`
-//   );
-//   if (!resp.ok) {
-//     console.error(resp.statusText);
-//     return;
-//   }
-//   const data = yield resp.json();
-//   if (!!data && data.length > 0) {
-//     yield put({ type: "FETCH_USER_SUCCESS", username, user: data[0] });
-//   } else {
-//     yield put({ type: "FETCH_USER_FAILED", username });
-//   }
-// }
+// MIT License
+//
+// Copyright (c) 2022 Gan Tu
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 export function* watchCubeApp() {
-  //   yield takeEvery("FETCH_USER", fetchUser);
+  // nothing yet
 }
